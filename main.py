b31ffb9894e92821699521279dd3d764f96e53ed
Aqsa Saleheene <angieclrk21@gmail.com>
2022-05-01 01:13:26 -0400

    HW Done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Create your own adventure with python here
 
-def play():
+def build():
     x = input(
         "What life do you want? Mountains, Beaches or live in a van and travel the world?")
 
@@ -12,24 +12,24 @@ def play():
                 x = input(
                     'Lets edit your van to your liking before traveling the roads! Do you want a kitchen? Yes or No')
                 if x == 'No':
-                    play()
+                    build()
             else:
                 x = input(
                     'How about a griddle in the way back of your van? Yes or No')
                 if x == 'No':
-                    play()
+                    build()
         else:
             x = input('Do you want a shower in your Van? Yes or No')
             if x == "Yes":
                 x = input(
                     'Good Idea! Sink? Yes or No')
                 if x == 'Yes':
-                    play()
+                    build()
             else:
                 x = input(
                     'Plumbing? Yes or No')
                 if x == 'Yes':
-                    play()
+                    build()
     else:
         x = input("Fold up bed or Fulltime bed?")
         if x == "Fold up bed":
@@ -38,12 +38,12 @@ def play():
                 x = input(
                     'Yellow lights or White?')
                 if x == 'White':
-                    play()
+                    build()
             else:
                 x = input(
                     'Lamps ars so cute. Are you sure of it in the Van though? Yes or No')
                 if x == 'Yes':
-                    play()
+                    build()
         else:
             x = input(
                 'Would you like desk or no desk to work from home? Or in this case...Work from Van! Ha!')
@@ -51,18 +51,32 @@ def play():
                 x = input(
                     'Would you like an outlet? Yes or No')
                 if x == 'Yes':
-                    play()
+                    build()
             else:
                 x = input(
                     'Would you like a map since you are visiting all 50 States? Yes or No')
                 if x == 'Yes':
-                    play()
+                    build()
            
                  
-       
+    
+             again=str(input("Do you want to play again? Yes or No "))
+             if again == "no":
+             play = False
+
+
+
+
+             # wasnt sure which one was right?
+             #def again():
+            #if input("Continue? Yes or No") == 'Yes':
+         #build()
+    #else:
+       # exit
+
 
 
           
 
 
-play()
+build()
