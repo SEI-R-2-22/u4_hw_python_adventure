1b18dbe09e5f8f6b599e067e02bc49cefa7beef0
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-05-02 08:42:18 -0400

    python adventrure

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
 # Create your own adventure with python here
+# from cmath import isnan
+from math import*
+
+
+gameover = False
+
+while (not gameover):
+    name = input('Please tell me your name.\n')
+
+    if (len(name) > 5):
+        print('Your name is very long. 😞\n')
+        name = input('Perhaps make it less than 6 characters. Please choose a new name\n')
+        if (len(name) > 5):
+            print('you mock me with long names you have failed.\n')
+            gameover = True
+            #endpoint 1
+        elif(not name[0].isupper()):
+            print('Not enough dignity to capitalize your name. you have failed me')
+            gameover = True
+            # endpoint 2
+        else:
+            print('Very well ' + name + 'let\'s go')
+    elif(not name[0].isupper()):
+            print('Not enough dignity to capitalize your name. you have failed me')
+            gameover = True
+            # endpoint 3
+    else:
+        print('Very well ' + name + 'let\'s go')
+
+    if(not gameover):
+        age = input('Now that I know who you are '+ name +', how old are you?\n')
+
+
+
+        if(int(age) == 1):
+            print('Holy Moly! You must be a genius + 10 points')
+        elif(int(age) > 21):
+            print('Lets grab a drink')
+        else:
+            print('to young for some mead')
