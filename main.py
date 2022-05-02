94a512d2ddf853198d03b347317487db1031d275
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-05-02 09:01:10 -0400

    python adventure

--- main.py
+++ main.py
@@ -31,11 +31,45 @@ while (not gameover):
     if(not gameover):
         age = input('Now that I know who you are '+ name +', how old are you?\n')
 
-
-
         if(int(age) == 1):
             print('Holy Moly! You must be a genius + 10 points')
         elif(int(age) > 21):
             print('Lets grab a drink')
         else:
-            print('to young for some mead')
+            print('to young for some mead or you used letters or something!')
+
+    play = input('Do you want to play a game?(y/n)')
+    if (play == 'n'):
+        print('then why are you here?')
+        gameover = True
+    else: print('I will take that as a yes')
+
+    play = input('What game do you want to play?')
+
+    if(play == 'catan' or 'Catan'):
+        print ('one of my favorites!')
+        print('you win!')
+        gameover = True
+    else: 
+        print('I dont like that game')
+        print ('you lose')
+        gameover = True
+
+
+
+
+
+
+    if(gameover):
+        print(' 😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔')
+        print(' 😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔')
+        print(' 😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔')
+        print(' 😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔')
+        print('The game is over')
+        print(' 😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔')
+        print(' 😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔')
+        print(' 😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔')
+
+        playagain = input('Would you like to play again?(y/n)')
+        if (playagain == 'y'):
+            gameover= False
